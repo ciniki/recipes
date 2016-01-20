52c3c3f5e4319fd29c1568131fec9220210e55af
Andrew Rivett <veggiefrog@gmail.com>
2016-01-20 17:08:12 -0500

    Added new hooks/webOptions

---
+++ hooks/webOptions.php
@@ -0,0 +1,46 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the list of options for the module that can be set for the website.
+//
+// Arguments
+// ---------
+// ciniki:
+// settings:		The web settings structure.
+// business_id:		The ID of the business to get options for.
+//
+// args:			The possible arguments for profiles
+//
+//
+// Returns
+// -------
+//
+function ciniki_recipes_hooks_webOptions(&$ciniki, $business_id, $args) {
+
+	//
+	// Check to make sure the module is enabled
+	//
+	if( !isset($ciniki['business']['modules']['ciniki.recipes']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3049', 'msg'=>"I'm sorry, the page you requested does not exist."));
+	}
+
+	//
+	// Get the settings from the database
+	//
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbDetailsQueryDash');
+	$rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_web_settings', 'business_id', $business_id, 'ciniki.web', 'settings', 'page-recipes');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['settings']) ) {
+		$settings = array();
+	} else {
+		$settings = $rc['settings'];
+	}
+
+	$pages['ciniki.recipes'] = array('name'=>'Artist Profiles', 'options'=>array());
+
+	return array('stat'=>'ok', 'pages'=>$pages);
+}
+?>
